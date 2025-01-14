969f98f112cc43dcca9ca6a07f2777e6ecba32ec
Pavel-Sobolev7 <pavel_sobolev_98@mail.ru>
2025-01-14 18:12:30 +0300

    feat: finish 05 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -6,3 +6,25 @@ const bundleFilePath = path.join(__dirname, 'project-dist', 'bundle.css');
 
 const bundleStream = fs.createWriteStream(bundleFilePath);
 
+fs.readdir(stylesFolderPath, (err, files) => {
+    if (err) {
+        console.error('Error reading the styles folder:', err);
+        return;
+    }
+
+    files.forEach(file => {
+        const filePath = path.join(stylesFolderPath, file);
+
+        if (path.extname(file) === '.css') {
+            const readStream = fs.createReadStream(filePath, 'utf8');
+
+            readStream.on('data', chunk => {
+                bundleStream.write(chunk + '\n');
+            });
+
+            readStream.on('error', err => {
+                console.error('Error reading CSS file:', err);
+            });
+        }
+    });
+});
