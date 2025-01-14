7202b57a77d2395bc0373c0a4a3011a8a40a5986
Pavel-Sobolev7 <pavel_sobolev_98@mail.ru>
2025-01-14 16:32:09 +0300

    feat: add display of information about files in folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,7 +5,20 @@ const path = require('path');
     try {
         const folderPath = path.join(__dirname, 'secret-folder');
 
+        const files = await fs.readdir(folderPath, { withFileTypes: true });
+
+        for (const file of files) {
+            if (!file.isFile()) continue;
+            const filePath = path.join(folderPath, file.name);
+            const stats = await fs.stat(filePath);
+
+            const fileName = path.parse(file.name).name;
+            const fileExt = path.extname(file.name).slice(1);
+            const fileSize = (stats.size / 1024).toFixed(3);
+
+            console.log(`${fileName} - ${fileExt} - ${fileSize}kb`);
+        }
     } catch (err) {
-        console.error('Error reading:', err.message);
+        console.error('Error reading the folder:', err.message);
     }
-});
+})();
