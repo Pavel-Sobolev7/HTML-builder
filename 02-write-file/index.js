d7f71fc3a5c4845d70a597b8f622f90d0cbe7926
Pavel-Sobolev7 <pavel_sobolev_98@mail.ru>
2025-01-14 14:49:40 +0300

    feat: add stream for recording

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,2 +1,5 @@
 const fs = require('fs');
 const readLine = require('readline');
+
+const filePath = './02-write-file/text.txt';
+const writableStream = fs.createWriteStream(filePath, { flags: 'a' });
