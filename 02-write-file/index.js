bb0b30b8778f85929de941c7b7d19e15286896c4
Pavel-Sobolev7 <pavel_sobolev_98@mail.ru>
2025-01-14 15:10:51 +0300

    feat: create a stream for recording

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,3 +3,25 @@ const readLine = require('readline');
 
 const filePath = './02-write-file/text.txt';
 const writableStream = fs.createWriteStream(filePath, { flags: 'a' });
+
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log('Enter your text:');
+
+rl.on('line', (input) => {
+    if (input.trim() === 'cancel') {
+        console.log('Goodbye !');
+        rl.close();
+    } else {
+        writableStream.write(`${input}\n`);
+        console.log('The text recorded. Enter text or "cancel" to exit');
+    }
+});
+
+process.on('SIGINT', () => {
+    console.log('\nGoodbye !');
+    rl.close();
+});
