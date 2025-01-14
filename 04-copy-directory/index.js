0b03eff3f67bd822873b3d0856ebe59c04b33b50
Pavel-Sobolev7 <pavel_sobolev_98@mail.ru>
2025-01-14 17:31:11 +0300

    feat: add copy directory function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,8 +6,25 @@ async function copyDir() {
         const srcFolder = path.join(__dirname, 'files');
         const destFolder = path.join(__dirname, 'files-copy');
 
+        await fs.rm(destFolder, { recursive: true, force: true });
+        await fs.mkdir(destFolder, { recursive: true });
 
+        const items = await fs.readdir(srcFolder, { withFileTypes: true });
+
+        for (const item of items) {
+            const srcPath = path.join(srcFolder, item.name);
+            const destPath = path.join(destFolder, item.name);
+
+            if (item.isDirectory()) {
+                await copyDir(srcPath, destPath);
+            } else if (item.isFile()) {
+                await fs.copyFile(srcPath, destPath);
+            }
+        }
+        console.log('Directory copied successfully!');
     } catch (err) {
         console.error('Error copying directory:', err.message);
     }
 }
+
+copyDir();
